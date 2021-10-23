4c645f71ea1e5f567c77497ada03185f6a46159c
MargoG <88088029+MargoG197@users.noreply.github.com>
2021-10-23 11:33:07 +0300

    added function

--- Task_8.js
+++ Task_8.js
@@ -7,15 +7,28 @@ myFirstMap.set("China capital", "Beijing");
 myFirstMap.set("Malasia capital", "Kuala Lumpur");
 myFirstMap.set("Nepal capital", "Katmandu");
 
+let myFirstMap = new Map();
+myFirstMap.set("China capital", "Beijing");
+myFirstMap.set("Malasia capital", "Kuala Lumpur");
+myFirstMap.set("Nepal capital", "Katmandu");
+
+function countryCapital(){
+  for (let[key, value] of myFirstMap){
+console.log('Key - ' + key + ', Prop - ' + value);
+  }
+}
+countryCapital();
+
+
 //console.log(myFirstMap.get("China capital"));
 //console.log(myFirstMap.size);
 
-for (let country of myFirstMap.keys()){
-  console.log("Key - " + country);
-}
-for(let capital of myFirstMap.values()){
-  console.log("Prop - " + capital);
-}
+//for (let country of myFirstMap.keys()){
+//  console.log("Key - " + country);
+//}
+//for(let capital of myFirstMap.values()){
+//  console.log("Prop - " + capital);
+//}
 //for (let elem of myFirstMap){
 //  console.log(elem);
 //}
